f68587e84c3425eb101b576415b04b2807df1153
rhulraj <rahulanku51@gmail.com>
2024-09-08 00:25:29 +0530

    Completed gitAndGithub assignment

--- script.js
+++ script.js
@@ -1,13 +1,11 @@
 function Greeting(name){
      return `Welcome ${name}`
 }
-<<<<<<< HEAD
 Greeting("rahul raj")
 
 const result2 = Greeting("Ayush");
 console.log(result2);
-=======
+
 const result = Greeting("rahul raj");
 
-console.log(result)
->>>>>>> feature-update
+console.log(result);
